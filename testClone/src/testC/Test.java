c5a21d8ce99aee8341ca1443a7ab3d3a609082d8
Kris Leenaerts <kris.leenaerts@pragmaticsoftwaresolutions.org>
2014-09-27 16:32:07 +0200

    test
    test

---
+++ testClone/src/testC/Test.java
@@ -0,0 +1,20 @@
+/**
+ * Test
+ *
+ * 27 Sep 2014
+ *
+ * Copyright (c) Pragmatic Software Solutions. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Pragmatic Software Solutions BVBA. ("Confidential Information").
+ * It may not be copied or reproduced in any manner without the express written permission of Pragmatic Software Solutions BVBA.
+ *
+ */
+package testC;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
